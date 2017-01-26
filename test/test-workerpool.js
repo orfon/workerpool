e2a3eff6411bd113d92d666eee2af10c3b2e8976
Philipp Naderer <philipp.naderer@gmail.com>
2017-01-26 21:00:39 +0100

    Applied robert’s patch

--- test/test-workerpool.js
+++ test/test-workerpool.js
@@ -1,23 +1,25 @@
 const assert = require("assert");
 
 const {Semaphore} = require("ringo/concurrent");
-const {WorkerPool} = require("../lib/workerpool");
+const WorkerPool = require("../lib/workerpool");
 
 exports.testWorkerPool = function() {
     const sem = new Semaphore(0);
-    let pool = new WorkerPool(module.resolve("./test-workerpool"), 5, function(event) {
+    let pool = new WorkerPool(module.resolve("./test-workerpool"), 5);
+    pool.onmessage = function(event) {
         assert.isTrue(event.data.count > 0);
-    }, function(error) {
+    };
+    pool.onerror = function(error) {
         assert.fail("Error! " + error);
-    });
+    };
 
-    assert.strictEqual(pool.postMessage({ "count": 1, "sem": sem }, true), 0);
-    assert.strictEqual(pool.postMessage({ "count": 2, "sem": sem }, true), 1);
-    assert.strictEqual(pool.postMessage({ "count": 3, "sem": sem }, true), 2);
-    assert.strictEqual(pool.postMessage({ "count": 4, "sem": sem }, true), 3);
-    assert.strictEqual(pool.postMessage({ "count": 5, "sem": sem }, true), 4);
-    assert.strictEqual(pool.postMessage({ "count": 6, "sem": sem }, true), 0);
-    assert.strictEqual(pool.postMessage({ "count": 7, "sem": sem }, true), 1);
+    assert.strictEqual(pool.postMessage({ "count": 1, "sem": sem }), 0);
+    assert.strictEqual(pool.postMessage({ "count": 2, "sem": sem }), 1);
+    assert.strictEqual(pool.postMessage({ "count": 3, "sem": sem }), 2);
+    assert.strictEqual(pool.postMessage({ "count": 4, "sem": sem }), 3);
+    assert.strictEqual(pool.postMessage({ "count": 5, "sem": sem }), 4);
+    assert.strictEqual(pool.postMessage({ "count": 6, "sem": sem }), 0);
+    assert.strictEqual(pool.postMessage({ "count": 7, "sem": sem }), 1);
 
     if (!sem.tryWait(250, 7)) {
         assert.fail("Semaphore timed out!");
